8576bd7f5c90f913c3b4a40235dc422efe0f886a
蓝田_Loto <shorfng@126.com>
2018-03-28 18:03:27 +0800

    完成后台首页展示、点击后台功能跳转到相关页面的功能

---
+++ e3mall_manager_web/src/main/java/com/loto/e3mall/controller/PageController.java
@@ -0,0 +1,23 @@
+package com.loto.e3mall.controller;
+
+// 页面跳转Controller
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	// 展示后台首页
+	@RequestMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+
+	// 点击后台功能跳转到相关页面
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page) {
+		return page;
+	}
+}
